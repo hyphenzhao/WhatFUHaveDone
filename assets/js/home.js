8b5e517d6a4b099aca240a766aaedd82906f9ba8
hyphenzhao <haifeng.zhao@wearable-aac.com>
2026-06-25 16:54:06 +0800

    fix: yun.getStartAge() → yun.getStart()

--- assets/js/home.js
+++ assets/js/home.js
@@ -137,7 +137,7 @@ async function renderBaziPillars(dateStr) {
         const gender = p.gender === '女' ? 0 : 1;
         const yun = birthEc.getYun(gender);
         const ageYears = (selDate - bd) / (365.25 * 24 * 3600 * 1000);
-        const startAge = yun.getStartAge();
+        const startAge = yun.getStart();
 
         let daYunIdx = -1;
         for (let i = 0; i < 10; i++) {
